b5d4fbe6859fed1f591ecaeca96309f60adaf42d
prakrutigit <24ce096@charusat.edu.in>
2024-10-17 00:04:42 +0530

    Add files via upload

---
+++ prac2.c
@@ -0,0 +1,19 @@
+/*programmer- 24ce096 pethani prakruti
+  date of modification - 15 july
+  aim-calculating the area and perimeter of rectangle*/
+#include <stdio.h>
+ int main(){
+    int Lenght,Breadth,Area,Perimeter;
+    //assign the value given
+    Lenght=50;
+    Breadth=30;
+    //formula of calculating area
+    Area=Lenght*Breadth;
+    //formula of calculating perimeter
+    Perimeter=2*(Lenght+Breadth);
+    //displaying answer
+     printf("the area of rectangle is %d\n",Area);
+     printf("the perimeter of rectangle is %d\n\n",Perimeter);
+     printf("24CE096_PETHANI PRAKRUTI\n");
+
+}
